228f7dfd343539ec52f16a81bf23c1fd181d1861
Lucas Jensen <jensenlucasnahuel@gmail.com>
2026-08-19 18:48:52 -0300

    Agregar archivos de proyecto.

---
+++ Practico 2/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        LNya = New Label()
+        LModificar = New Label()
+        LDni = New Label()
+        LApellido = New Label()
+        LNombre = New Label()
+        TDni = New TextBox()
+        TApellido = New TextBox()
+        TNombre = New TextBox()
+        BGuardar = New Button()
+        BEliminar = New Button()
+        SuspendLayout()
+        ' 
+        ' LNya
+        ' 
+        LNya.AutoSize = True
+        LNya.Location = New Point(50, 39)
+        LNya.Name = "LNya"
+        LNya.Size = New Size(107, 15)
+        LNya.TabIndex = 0
+        LNya.Text = "Nombre y Apellido"
+        ' 
+        ' LModificar
+        ' 
+        LModificar.AutoSize = True
+        LModificar.ForeColor = Color.Red
+        LModificar.Location = New Point(182, 39)
+        LModificar.Name = "LModificar"
+        LModificar.Size = New Size(58, 15)
+        LModificar.TabIndex = 1
+        LModificar.Text = "Modificar"
+        ' 
+        ' LDni
+        ' 
+        LDni.AutoSize = True
+        LDni.Location = New Point(50, 108)
+        LDni.Name = "LDni"
+        LDni.Size = New Size(27, 15)
+        LDni.TabIndex = 2
+        LDni.Text = "DNI"
+        ' 
+        ' LApellido
+        ' 
+        LApellido.AutoSize = True
+        LApellido.Location = New Point(50, 163)
+        LApellido.Name = "LApellido"
+        LApellido.Size = New Size(51, 15)
+        LApellido.TabIndex = 3
+        LApellido.Text = "Apellido"
+        ' 
+        ' LNombre
+        ' 
+        LNombre.AutoSize = True
+        LNombre.Location = New Point(50, 218)
+        LNombre.Name = "LNombre"
+        LNombre.Size = New Size(51, 15)
+        LNombre.TabIndex = 4
+        LNombre.Text = "Nombre"
+        ' 
+        ' TDni
+        ' 
+        TDni.Location = New Point(155, 108)
+        TDni.Name = "TDni"
+        TDni.Size = New Size(100, 23)
+        TDni.TabIndex = 5
+        ' 
+        ' TApellido
+        ' 
+        TApellido.Location = New Point(155, 163)
+        TApellido.Name = "TApellido"
+        TApellido.Size = New Size(100, 23)
+        TApellido.TabIndex = 6
+        ' 
+        ' TNombre
+        ' 
+        TNombre.Location = New Point(155, 218)
+        TNombre.Name = "TNombre"
+        TNombre.Size = New Size(100, 23)
+        TNombre.TabIndex = 7
+        ' 
+        ' BGuardar
+        ' 
+        BGuardar.Location = New Point(66, 300)
+        BGuardar.Name = "BGuardar"
+        BGuardar.Size = New Size(75, 23)
+        BGuardar.TabIndex = 8
+        BGuardar.Text = "Guardar"
+        BGuardar.UseVisualStyleBackColor = True
+        ' 
+        ' BEliminar
+        ' 
+        BEliminar.Location = New Point(165, 300)
+        BEliminar.Name = "BEliminar"
+        BEliminar.Size = New Size(75, 23)
+        BEliminar.TabIndex = 9
+        BEliminar.Text = "Eliminar"
+        BEliminar.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(354, 368)
+        Controls.Add(BEliminar)
+        Controls.Add(BGuardar)
+        Controls.Add(TNombre)
+        Controls.Add(TApellido)
+        Controls.Add(TDni)
+        Controls.Add(LNombre)
+        Controls.Add(LApellido)
+        Controls.Add(LDni)
+        Controls.Add(LModificar)
+        Controls.Add(LNya)
+        Name = "Form1"
+        Text = "Pequeño Formulario"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents LNya As Label
+    Friend WithEvents LModificar As Label
+    Friend WithEvents LDni As Label
+    Friend WithEvents LApellido As Label
+    Friend WithEvents LNombre As Label
+    Friend WithEvents TDni As TextBox
+    Friend WithEvents TApellido As TextBox
+    Friend WithEvents TNombre As TextBox
+    Friend WithEvents BGuardar As Button
+    Friend WithEvents BEliminar As Button
+
+End Class
